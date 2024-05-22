c12e4aadeec47f70a56e2f6d2cc99ba2a6994f47
Lukispo <lukagugava9@gmail.com>
2024-05-22 18:17:31 +0400

    second commit

---
+++ day 2/lesson2.py
@@ -0,0 +1,28 @@
+1.
+name = "Luka"
+Last_name = "gugava"
+age = 12
+hobby = "playing_chess"
+country = "georgia"
+
+print(name, Last_name, age, hobby, country)
+
+2.
+Hello = "Hi"
+I = "I"
+Am = "Am"
+name = "Luka"
+
+print(Hello + " " + I + " " + Am + " " + name)
+
+3.
+print(5+5)
+print(102+98)
+print(-1 - 5)
+print(50 - 25)
+print(-5 * 5)
+print(5 * 5)
+print(99 // 11)
+print(48 // 7)
+print(5 ** 1)
+print(503 ** 104)
