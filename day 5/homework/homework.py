4a6af0e6abbcbd43cd468a3c5f2b1ae885f87945
Lukispo <lukagugava9@gmail.com>
2024-05-29 18:54:49 +0400

    commit

---
+++ day 5/homework/homework.py
@@ -0,0 +1,21 @@
+# დავალება 1.
+
+# უნდა შევინახოთ ინფორმაცია ცვლადების გამოყენებით და დავპრინტოთ წინადადების სახით.
+
+
+name = "Luka"
+last_name = "Gugava"
+hobby = "Football"
+age = 12
+country = "georgia"
+
+# შევქმენით ცვლადები, ახლა ჩვენ ისინი უნდა დავპრინტოთ 
+
+
+print(name, last_name, hobby, age, country)
+
+
+
+
+
+
