15864b2e5cd570e1535f754e932e1fa303e4c490
Lukispo <lukagugava9@gmail.com>
2024-06-09 20:30:37 +0400

    commit

---
+++ day 7/Classwork/classwork.py
@@ -0,0 +1,13 @@
+salary = 150000  # first salary
+pay_raise = 10000  # amount of raise
+new_salary = salary + pay_raise # last salary
+print(new_salary)
+
+
+
+
+name = input("What is your name: ")
+last_name = input("What is your last name: ")
+age = input("how old are you: ")
+country = input("which country do you live: ")
+print('My name is', name, last_name, 'i am', age, 'years old', 'I live in', country)
---
+++ day 7/Homework/homework.py
@@ -0,0 +1,50 @@
+country = "Georgia"
+name = "Luka"
+Last_Name = "Gugava"
+Hobbies = "Chess and football"
+
+
+
+
+
+
+
+
+input("MOM: Hey son, i got a new notification from GOA:")
+print("ME: what is it")
+input("MOM: It says that big present will be sent home for everyone:")
+print("ME: I wonder what the present will be")
+print("MOM: Yeah same.")
+
+input("*5 hours have went by*")
+
+input("MOM: son! somebodys at our door, go open it:")
+print("ME: alright mom!")
+
+
+# at the door
+
+input("*opening the door*")
+
+input("ME: Oh my gosh Nika keshelava is that you?")
+input("Chad Nika: Yes its me, i need to verify that i got the right person to give this precious gift to:")
+input("ME: sure, ask me anything")
+input("Chad Nika : 1. what is your name:")
+print(name)
+input("Chad Nika: 2. What is your Last name:")
+print(Last_Name)
+input("Chad Nika: 3.what are your hobbies:")
+print(Hobbies)
+input("Chad Nika: 4.which academy is best for programming?:")
+print("GOA")
+
+
+input("Chad Nika: alright, you have passed the test, you may have your present now")
+print("ME: Oh my gosh its *Bus kvercxebi*, thank you so much")
+input("Chad Nika: Dissapears")
+
+
+
+
+
+
