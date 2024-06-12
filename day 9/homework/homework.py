9f6b14a5305869b85282b99813cb0248617344aa
Lukispo <lukagugava9@gmail.com>
2024-06-12 21:13:37 +0400

    commit

---
+++ day 9/homework/homework.py
@@ -0,0 +1,50 @@
+#    1.
+
+
+num1 = int(input("please enter number 1: "))
+num2 = int(input("please enter number 2: "))
+
+
+print(num1 + num2)
+
+
+#    2.
+
+
+height = int(input("please enter height: "))
+width = int(input("please enter width: "))
+
+print(height * width)
+
+
+
+#    3.
+
+
+number1 = int(input("Please enter number: "))
+
+
+print(number1 * 2)
+
+
+#    4. 
+
+
+height1 = int(input("please enter height: "))
+width = int(input("please enter width: "))
+
+
+print(height + height + width + width)
+
+
+
+#    5.
+
+
+num3 = int(input("please enter number: "))
+
+
+print(num3 ** 2)
+
+
+# homework done.
