5676b8a201b7487f0c142ac43ef4e3debe261362
Lukispo <lukagugava9@gmail.com>
2024-07-21 23:25:59 +0400

    commit

---
+++ day 19/homework/homework.py
@@ -0,0 +1,9 @@
+for i in range(126, 0, -1):
+    print(i)
+
+
+for b in range(126, 0, -2):
+    print(b)
+
+for z in range(126, 0, -3):
+    print(z)
---
+++ day 19/homework/homework1.py
@@ -0,0 +1,2 @@
+for i in range(1, 48):
+    print("100 +", i, "=", i + 100)
