e4247eb1c0c257c8b7d38c5a31ef6a2f379e9229
Lukispo <lukagugava9@gmail.com>
2024-08-16 01:02:55 +0400

    commit

---
+++ day 27/homework/homework1.py
@@ -0,0 +1,7 @@
+something = [10, 438, "hello", 587.3, "world", "erm", "print", True, False, "what"]
+something[0] = 11
+something[11] = "wot"
+something[5] = "the"
+something[3] = "sigma"
+something[1] = "flip"
+print(something)
---
+++ day 27/homework/homework2.py
@@ -0,0 +1,3 @@
+numbers = [50, 25, 100,]
+sum = sum(numbers)
+print(sum)
